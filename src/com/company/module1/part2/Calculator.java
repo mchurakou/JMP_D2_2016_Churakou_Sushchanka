95e4c0eca6d00fa3939688b7382119037bee6da4
Hanna Sushchanka <asus200786@gmail.com>
2016-07-10 14:34:53 +0300

    task1_module1: App contains violation. Status: In progress.

--- src/com/company/module1/part2/Calculator.java
+++ src/com/company/module1/part2/Calculator.java
@@ -12,26 +12,32 @@ public class Calculator {
     public static void main (String[] args) {
         char operator;
         double leftValue, rightValue, result;
+        boolean quit = false;
         Scanner sc = new Scanner(System.in);
         try {
-            System.out.println("Enter the first number: ");
+            System.out.println("Enter the number: ");
             leftValue = sc.nextDouble();
-            System.out.println("Enter the operation: ");
-            operator = sc.next().charAt(0);
-            System.out.println("Enter the second number: ");
-            rightValue = sc.nextDouble();
-            SimpleCalcFunctions calcFunctions = new SimpleCalcFunctions(operator);
-            ArithmeticOperationsI opc = calcFunctions.getFunctions().get(operator);
-            if (opc != null) {
-                result = opc.resultOfOperation(leftValue, rightValue);
-                System.out.println("Result: " + result);
-            } else {
-                System.out.println("Error: Unknown operator");
+            while (!quit) {
+                System.out.println("Enter the operation: ");
+                operator = sc.next().charAt(0);
+                System.out.println("Enter the next number: ");
+                rightValue = sc.nextDouble();
+                SimpleCalcFunctions calcFunctions = new SimpleCalcFunctions(operator);
+                ArithmeticOperationsI opc = calcFunctions.getFunctions().get(operator);
+                if (opc != null) {
+                    result = opc.resultOfOperation(leftValue, rightValue);
+                    System.out.println("Result: " + result);
+                    leftValue = result;
+                } else {
+                    System.out.println("Error: Unknown operator");
+                    quit = true;
+                }
             }
         } catch (InputMismatchException e) {
-            System.out.println("Error: Unknown symbol");
-            sc.nextLine();
+            System.out.println("Error: Unknown symbol.");
+        } finally {
+            sc.close();
         }
-        sc.close();
+
     }
 }
