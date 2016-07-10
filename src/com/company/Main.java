32c35e9656f95ba5b6f87629b7ce79ef530626ce
Hanna Sushchanka <asus200786@gmail.com>
2016-07-10 12:22:41 +0300

    task1_module1: App contains violation. Status: In progress.

---
+++ src/com/company/Main.java
@@ -0,0 +1,37 @@
+package com.company;
+
+/**
+ * Created by alt-hanny on 10.07.2016.
+ */
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        double inputX, inputY;
+        double answer = 0;
+        char operation;
+        InputStream input =  System.in;
+        Scanner sc = new Scanner(input);
+        System.out.println("Enter the first number: ");
+        inputX = sc.nextDouble();
+        System.out.println("Enter the operation: ");
+        operation = sc.next().charAt(0);
+        System.out.println("Enter the second number: ");
+        inputY = sc.nextDouble();
+        Operations ops = new Operations();
+        switch (operation) {
+            case '+': answer = ops.summation(inputX, inputY);
+                break;
+            case '-': answer = ops.subtraction(inputX, inputY);
+                break;
+            case '*': answer = ops.multiplication(inputX, inputY);
+                break;
+            case '/': answer = ops.division(inputX, inputY);
+                break;
+
+            default: break;
+        }
+        System.out.println("Result: " + answer);
+    }
+}
---
+++ src/com/company/Operations.java
@@ -0,0 +1,27 @@
+package com.company;
+
+/**
+ * Created by alt-hanny on 10.07.2016.
+ */
+public class Operations {
+
+    public double summation (double x, double y) {
+        double result = x + y;
+        return result;
+    }
+
+    public double subtraction (double x, double y) {
+        double result = x - y;
+        return result;
+    }
+
+    public double multiplication (double x, double y) {
+        double result = x * y;
+        return result;
+    }
+
+    public double division (double x, double y) {
+        double result = x / y;
+        return result;
+    }
+}
