bd56c8947f76a354c7989a16474b185dce96b189
Hanna Sushchanka <asus200786@gmail.com>
2016-08-16 06:56:00 +0300

    module6

--- module6/src/structural_patterns/SelectionCharacters.java
+++ module6/src/structural_patterns/SelectionCharacters.java
@@ -1,34 +1,79 @@
 package structural_patterns;
 
 import structural_patterns.characters.HealerAdapter;
-import structural_patterns.characters.fighters.Fighter;
-import structural_patterns.characters.fighters.Mage;
-import structural_patterns.characters.fighters.Monk;
-import structural_patterns.characters.fighters.Warrior;
+import structural_patterns.characters.fighters.*;
 import structural_patterns.characters.healers.Healer;
-import structural_patterns.characters.healers.SimpleHealer;
+import structural_patterns.characters.healers.Prist;
+import structural_patterns.characters.races.Elf;
 import structural_patterns.characters.races.Human;
 import structural_patterns.characters.races.Race;
+
+import java.util.Scanner;
 
 /**
  * Created by alt-hanny on 14.08.2016.
  */
 public class SelectionCharacters {
     private Race race;
+    private Fighter fighter;
 
     public void charSelection() {
-        race = new Human();
+        boolean userChooseDone;
+        try (Scanner sc = new Scanner(System.in)){
+            System.out.println("Please, choose the race of the character: \n 1 - Human; \n 2 - Elf;");
+            do{
+                String selection = sc.nextLine();
+                switch (selection) {
+                    case "1":
+                        race = new Human();
+                        userChooseDone = Boolean.TRUE;
+                        break;
+                    case "2":
+                        race = new Elf();
+                        userChooseDone = Boolean.TRUE;
+                        break;
+                    default:
+                        System.out.println("Please, try again.");
+                        userChooseDone = Boolean.FALSE;
+                }
+            } while (!userChooseDone);
         fighterSelection(race);
+        }
     }
 
     public void fighterSelection(Race race) {
-        Fighter fighter = new Monk();
-        Mage mage = new Mage(fighter, race);
-        mage.fight();
-        Warrior warrior = new Warrior(fighter, race);
-        warrior.fight();
-        Healer healer = new SimpleHealer();
-        Fighter healerAdapter = new HealerAdapter(healer, race);
-        healerAdapter.autofight();
+        boolean userChooseDone;
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Please, choose the class of the character: \n 1 - Monk; \n 2 - Mage; \n 3 - Warrior; \n 4 - Prist");
+            do {
+                String selection = sc.nextLine();
+                switch (selection) {
+                    case "1":
+                        fighter = new Monk();
+                        userChooseDone = Boolean.TRUE;
+                        break;
+                    case "2":
+                        Mage mage = new Mage(fighter, race);
+                        mage.fight();
+                        userChooseDone = Boolean.TRUE;
+                        break;
+                    case "3":
+                        fighter = new Monk();
+                        Warrior warrior = new Warrior(fighter, race);
+                        warrior.fight();
+                        userChooseDone = Boolean.TRUE;
+                        break;
+                    case "4":
+                        Healer healer = new Prist();
+                        Fighter healerAdapter = new HealerAdapter(healer, race);
+                        healerAdapter.autofight();
+                        userChooseDone = Boolean.TRUE;
+                        break;
+                    default:
+                        System.out.println("Please, try again.");
+                        userChooseDone = Boolean.FALSE;
+                }
+            } while (!userChooseDone);
+        }
     }
 }
--- module6/src/structural_patterns/characters/healers/Prist.java
+++ module6/src/structural_patterns/characters/healers/Prist.java
@@ -3,9 +3,9 @@ package structural_patterns.characters.healers;
 /**
  * Created by alt-hanny on 14.08.2016.
  */
-public class SimpleHealer extends Healer {
+public class Prist extends Healer {
     @Override
     public void heal() {
-        System.out.println("Heal yourself");
+        System.out.println("Heal the team member.");
     }
 }
