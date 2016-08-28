7150247ccb32ba5bf4d5761c65708c04a863586c
Hanna Sushchanka <asus200786@gmail.com>
2016-08-28 13:13:30 +0300

    module8

---
+++ module8/src/com/company/Deadlock.java
@@ -0,0 +1,36 @@
+package com.company;
+
+/**
+ * Created by alt-hanny on 28.08.2016.
+ */
+public class Deadlock {
+    static class Friend {
+        private final String name;
+        Friend(String name) {
+            this.name = name;
+        }
+        String getName() {
+            return this.name;
+        }
+        synchronized void bow(Friend bower) {
+            System.out.format("%s: %s"
+                            + "  has bowed to me!%n",
+                    this.name, bower.getName());
+            bower.bowBack(this);
+        }
+        synchronized void bowBack(Friend bower) {
+            System.out.format("%s: %s"
+                            + " has bowed back to me!%n",
+                    this.name, bower.getName());
+        }
+    }
+
+    public static void main(String[] args) {
+        final Friend alphonse =
+                new Friend("Alphonse");
+        final Friend gaston =
+                new Friend("Gaston");
+        new Thread(() -> { alphonse.bow(gaston); }).start();
+        new Thread(() -> { gaston.bow(alphonse); }).start();
+    }
+}
