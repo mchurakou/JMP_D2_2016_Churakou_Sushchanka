72696fe97e7f8e2cff7ef11547e3c85208c8c67e
Hanna Sushchanka <asus200786@gmail.com>
2016-10-12 02:08:28 +0300

    module14 maven

---
+++ module14_Maven/src/main/java/User.java
@@ -0,0 +1,26 @@
+/**
+ * Created by alt-hanny on 11.10.2016.
+ */
+public class User{
+    private String firstName;
+    private  String lastName;
+
+    public User() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
