ed97dca3f08b3029213254db31c4123f3b6357b0
Hanna Sushchanka <asus200786@gmail.com>
2016-10-18 23:40:33 +0300

    module15

--- module15/src/main/java/com/company/sushchanka/App.java
+++ module15/src/main/java/com/company/sushchanka/App.java
@@ -1,6 +1,5 @@
 package com.company.sushchanka;
 
-import com.company.sushchanka.beans.Task;
 import com.mongodb.*;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
@@ -8,27 +7,15 @@ import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 
-import java.net.UnknownHostException;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
+import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.List;
 
 import static com.mongodb.client.model.Filters.lt;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy MM dd");
-        Task task = new Task("AAAAAA", "x", LocalDate.parse("2016 07 02", formatter), LocalDate.parse("2016 07 02", formatter));
-        System.out.println( task );
-
         try (MongoClient mongoClient = new MongoClient("localhost", 27017)) {
             MongoDatabase db = mongoClient.getDatabase("module15");
             MongoCollection<Document> tasks = db.getCollection("tasks");
@@ -49,20 +36,31 @@ public class App
                 System.out.println(iterator.next().toString());
             }
 
+            System.out.println("Tasks count: "+ tasks.count());
+
+            System.out.println("+++++++++ Update task ++++++++++");
+            Document query = new Document("name", "B_2");
+            Document newDoc = new Document("name", "JJ");
+            Document updateDoc = new Document("$set", newDoc);
+            tasks.updateOne(query, updateDoc);
+
             System.out.println("+++++++++ Display overdue tasks +++++++++++");
-            FindIterable<Document> iterable1 = tasks.find(lt("deadLine", new Date()));
+            FindIterable<Document> iterable1 = tasks.find(lt("deadLine", new SimpleDateFormat("yyyy-MM-dd").format(new Date())));
             iterable1.forEach((Block <? super Document>)System.out::println);
 
+            /** Delete. */
+            System.out.println("+++++ Delete by name ++++++++");
+            Document searchQuery = new Document();
+            searchQuery.put("name", "B_4");
+            tasks.findOneAndDelete(searchQuery);
+
             /** Display all task one category. */
-            System.out.println("++++++++ Display task one category");
-            FindIterable<Document> iterable2 = tasks.find(new Document("category", "x"));
+            System.out.println("++++++++ Display task one category +++++++++++");
+            FindIterable<Document> iterable2 = tasks.find(new Document("category", "o"));
             iterable2.forEach((Block <? super Document>)System.out::println);
 
-            /** Delete */
-            System.out.println("+++++ Delete category ++++++++");
-            Document searchQuery = new Document();
-            searchQuery.put("category", "x");
-            tasks.findOneAndDelete(searchQuery);
+            tasks.drop();
+            System.out.println("Tasks dropped. Count: " + tasks.count());
         }
     }
 }
--- module15/src/main/java/com/company/sushchanka/Utils.java
+++
@@ -1,8 +0,0 @@
-package com.company.sushchanka;
-
-/**
- * Created by alt-hanny on 18.10.2016.
- */
-public class Utils {
-
-}
