b746524f4ac8b1990b527162dbbfce02e9f6250e
Hanna Sushchanka <asus200786@gmail.com>
2016-11-02 22:01:06 +0200

    REST

--- module17/src/main/java/com.company.springmvc/configuration/AppConfiguration.java
+++ module17/src/main/java/com.company.springmvc/configuration/AppConfiguration.java
@@ -14,6 +14,6 @@ import org.springframework.web.servlet.view.JstlView;
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.company.springmvc")
-public class HelloWorldConfiguration {
+public class AppConfiguration {
 
 }
